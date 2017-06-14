40d38ed3ccb4c59a9ac1889f3158aa87a707ae60
thibault62 <thibault.poly@viacesi.fr>
2017-06-14 11:22:45 +0200

    Test

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -9,7 +9,7 @@ import view.ViewFacade;
 /**
  * <h1>The Class Main.</h1>
  *
- * @author Jean-Aymeric DIET jadiet@cesi.fr test
+ * @author Jean-Aymeric DIET jadiet@cesi.fr test test2
  * @version 1.0
  */
 public abstract class Main {
